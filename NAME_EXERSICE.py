6a9056a17fd6b8b08e83cd706cd18e95a53db569
Mike Meeker <mmeeker@uber.com>
2017-03-20 14:29:22 -0700

    name exercise rv2

--- NAME_EXERSICE.py
+++ NAME_EXERSICE.py
@@ -3,5 +3,6 @@
 name1 = "mike"
 name2 = "gibson"
 name3 = "calvin"
+name4 = "dani"
 
-print"{:>30}{:>30}{:>30}".format(name1,name2,name3)
+print"{:>30}{:>30}{:>30}{:>30}".format(name1,name2,name3,name4)
