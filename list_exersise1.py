ab5abe54f879ec620ea77e73e36f9fe70b15cb36
Mike Meeker <mmeeker@uber.com>
2017-03-20 16:36:59 -0700

    list_exersise1

---
+++ list_exersise1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+my_list = ['hello', 'goodbye', 55.0, 66.5, 'oh no']
+
+my_list.append('hmmmmmmmm')
+
+my_list.append(56.7)
+
+my_list.pop(0)
+
+len(my_list)
+
+my_list.sort()
+
+print my_list
