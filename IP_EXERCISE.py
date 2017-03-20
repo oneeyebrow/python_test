496d6841967d6785b5f23a166b8eed70e65a45b9
Mike Meeker <mmeeker@uber.com>
2017-03-20 14:38:24 -0700

    ip exercise v2

--- IP_EXERCISE.py
+++ IP_EXERCISE.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-ip.addr = 192.168.255.255
+ip.addr = "192.168.255.255"
 
-octets = ip.addr.split(.)
+octets = "ip.addr.split(.)"
 
 print"{:<12}{:<12}{:<12}{:<12}".format(octets)
 
